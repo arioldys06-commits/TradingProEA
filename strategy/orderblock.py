596a7e215c792fdb39af7c91cf15175cf874f574
arioldys06-commits <arioldys06@gmail.com>
2026-06-24 17:49:56 -0400

    Add TradingProEA Elite strategy core

--- strategy/orderblock.py
+++ strategy/orderblock.py
@@ -1,1 +1,41 @@
+# strategy/orderblock.py
 
+def detect_orderblock(candles, direction):
+    if len(candles) < 5:
+        return {
+            "detected": False,
+            "type": None,
+            "zone": None
+        }
+
+    last_candles = candles[-5:]
+
+    if direction == "buy":
+        for candle in reversed(last_candles):
+            if candle["close"] < candle["open"]:
+                return {
+                    "detected": True,
+                    "type": "bullish_orderblock",
+                    "zone": {
+                        "top": round(candle["open"], 2),
+                        "bottom": round(candle["low"], 2)
+                    }
+                }
+
+    if direction == "sell":
+        for candle in reversed(last_candles):
+            if candle["close"] > candle["open"]:
+                return {
+                    "detected": True,
+                    "type": "bearish_orderblock",
+                    "zone": {
+                        "top": round(candle["high"], 2),
+                        "bottom": round(candle["open"], 2)
+                    }
+                }
+
+    return {
+        "detected": False,
+        "type": None,
+        "zone": None
+    }
