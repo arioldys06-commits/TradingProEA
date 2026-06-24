1b58472ebebac4b17e1e059ce76fe9f68b58f35e
arioldys06-commits <arioldys06@gmail.com>
2026-06-24 17:51:22 -0400

    Add TradingProEA Elite strategy core

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
+# config.py
 
+SYMBOL = "XAUUSD"
+
+TIMEFRAME_MAIN = "M15"
+TIMEFRAME_ENTRY = "M5"
+
+MIN_SCORE_TO_TRADE = 95
+
+MAX_TRADES_PER_DAY = 3
+MAX_LOSSES_PER_DAY = 2
+
+RISK_PER_TRADE_PERCENT = 1.0
+
+USE_NEWS_FILTER = False
+USE_SESSION_FILTER = True
+
+ALLOWED_SESSIONS = {
+    "london": {
+        "start": "03:00",
+        "end": "05:30"
+    },
+    "new_york": {
+        "start": "08:30",
+        "end": "11:00"
+    }
+}
